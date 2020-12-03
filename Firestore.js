847ea626793bbc0c5783ff024ceeda8ae08962d6
Somsubhra97 <somsubhrablasterjaxxmitra@gmail.com>
2020-12-03 19:13:01 +0530

    init:

--- Firestore.js
+++ Firestore.js
@@ -2,6 +2,21 @@ const list = document.querySelector('ul');
 const form = document.querySelector('form');
 let idx=0;
 
+// real-time listener
+db.collection('recipes').onSnapshot(snapshot => {//snapshot fetches an array if data stored
+
+  snapshot.docChanges().forEach(x => {
+     if(x.type === 'added'){
+
+      addRecipe(x.doc.data(), x.doc.id)
+    } else if (x.type === 'removed'){
+
+      deleteRecipe(idx);
+    }
+  });
+});
+
+//add data
 const addRecipe = (recipe, id) => {
   let time = recipe.created_at.toDate();
   let li=document.createElement('li');
@@ -10,7 +25,7 @@ const addRecipe = (recipe, id) => {
   <div>${recipe.title}</div>
   <div><small>${time}</small></div>
   <button .btn btn danger btn-sm my-2">delete</button>`
-  
+
   list.append(li);
   };
 
@@ -23,21 +38,8 @@ const deleteRecipe = (idx) => {
   });
 };
 
-// real-time listener
-db.collection('recipes').onSnapshot(snapshot => {//snapshot fetches an array if data stored
-  
-  snapshot.docChanges().forEach(x => {
-     if(x.type === 'added'){
-      
-      addRecipe(x.doc.data(), x.doc.id)
-    } else if (x.type === 'removed'){
-      
-      deleteRecipe(idx);
-    }
-  });
-});
 
-// save documents
+// form submition
 form.addEventListener('submit', e => {
   e.preventDefault();
   const now = new Date();
@@ -45,7 +47,7 @@ form.addEventListener('submit', e => {
     title: form.recipe.value,
     created_at: firebase.firestore.Timestamp.fromDate(now)
   };
-  db.collection('recipes').add(recipe).then(() => {    
+  db.collection('recipes').add(recipe).then(() => {
     form.reset();
   }).catch(err => {
     console.log(err);
